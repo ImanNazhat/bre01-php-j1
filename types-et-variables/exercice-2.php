35ff97066786c05fbbf75603ecde4491b020f191
ImanNazhat <emannzhat@gmail.com>
2024-01-10 10:39:56 +0000

    exercice 2 boucles et tableaux

--- types-et-variables/exercice-2.php
+++ types-et-variables/exercice-2.php
@@ -1,15 +1,16 @@
-Déclarez un tableau de nombres appelé `$nb_tab` qui contient des `int` et des `float` et affichez-le en utilisant `print_r`.
 
-Déclarez un tableau de nombres appelé `$str_tab` qui contient des `string` et affichez-le en utilisant `print_r`.
-
-La documentation de `print_r` : https://www.php.net/manual/fr/function.print-r.php
 
 <?php
 
 $nb_tab = [2 , 5.4 , 6 , 1.5];
 print_r($nb_tab);
 
+// $nb_tab = [
+//         'int' => '2', 'float' => '1.5'
+//   ];
+
 $str_tab = ["20" , "3" , "8"];
 print_r($str_tab);
 
 ?>
+
