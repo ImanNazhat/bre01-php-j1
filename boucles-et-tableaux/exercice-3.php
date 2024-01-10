b30c84b6bcce579ef497eb4df64aac9716bc3604
ImanNazhat <emannzhat@gmail.com>
2024-01-10 11:05:07 +0000

    exercice 3 boucles et tableaux

---
+++ boucles-et-tableaux/exercice-3.php
@@ -0,0 +1,14 @@
+
+
+
+<?php
+
+$user = [
+	"firstName" => "Barack",
+	"lastName" => "Obama"
+];
+
+$phrase = "Je suis {$user["firstName"]} {$user["lastName"]} , le 44ème président des USA. ";
+echo $phrase;
+
+?>
