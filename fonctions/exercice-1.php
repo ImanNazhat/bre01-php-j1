598c4fd5d668fa8755181bf7dfd842437bc600f2
ImanNazhat <emannzhat@gmail.com>
2024-01-10 13:43:16 +0000

    exercice 1 fonctions

---
+++ fonctions/exercice-1.php
@@ -0,0 +1,15 @@
+
+
+<?php
+
+function concat(string $par1 ,string $par2) : string 
+                    {
+                        return "$par1$par2";
+                    }
+
+        echo concat("Hello ", "World !<br>");
+        echo concat("À la ", "claire fontaine<br>");
+        echo concat("La vie, l'univers ", "et tout le reste<br>");
+?>
+
+
